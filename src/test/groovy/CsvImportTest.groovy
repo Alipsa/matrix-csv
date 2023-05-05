916ceb9a3ff2393bd161506a6e7de617c7c64cb5
per <per@alipsa.se>
2023-05-05 12:21:44 +0200

    add tests

--- src/test/groovy/CsvImportTest.groovy
+++ src/test/groovy/CsvImportTest.groovy
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test
 import se.alipsa.groovy.matrix.Matrix
 import se.alipsa.groovy.matrixcsv.CsvImporter
 
+import java.text.NumberFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
 import static org.junit.jupiter.api.Assertions.*
 
 class CsvImportTest {
@@ -15,6 +19,34 @@ class CsvImportTest {
     Matrix basic = CsvImporter.importCsv(url, format)
     assertEquals(4, basic.rowCount(), "Number of rows")
     assertEquals(['id', 'name', 'date', 'amount'], basic.columnNames(), "Column names")
-    assertEquals(['4', 'Arne', '2023-07-01', '222.99'], basic.row(3), "lastRow")
+    assertEquals(['4', 'Arne', '2023-07-01', '222.99'], basic.row(3), "last row")
+  }
+
+  @Test
+  void csvWithSemiColonsQuoteAndEmptyLine() {
+    URL url = getClass().getResource("/colonQuotesEmptyLine.csv")
+    CSVFormat format = CSVFormat.Builder.create()
+        .setTrim(true)
+        .setDelimiter(';')
+        .setIgnoreEmptyLines(true)
+        .setQuote('"' as Character)
+        .build()
+    Matrix matrix = CsvImporter.importCsv(url, format)
+    assertEquals(4, matrix.rowCount(), "Number of rows")
+    assertEquals(['id', 'name', 'date', 'amount'], matrix.columnNames(), "Column names")
+    assertEquals(['4', 'Arne', '2023-Jul-01', '222,99'], matrix.row(3), "last row")
+
+    Matrix table = matrix.convert(
+        ["id": Integer,
+         "name": String,
+         "date": LocalDate,
+         "amount": BigDecimal
+        ],
+        DateTimeFormatter.ofPattern("yyyy-MMM-dd"),
+        NumberFormat.getInstance(Locale.GERMANY)
+    )
+    assertEquals(4, table.rowCount(), "Number of rows")
+    assertEquals(['id', 'name', 'date', 'amount'], table.columnNames(), "Column names")
+    assertEquals([4, 'Arne', LocalDate.parse('2023-07-01'), 222.99], table.row(3), "last row")
   }
 }
